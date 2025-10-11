b96cae4ada2306d8f3e94b4930a6e3c746f7dabc
Lemon <lemonhe@tudelft.nl>
2025-10-11 23:44:13 +0200

    Add: save embeddings to avoid duplicate calculation

--- Project-RecSys/utils.py
+++ Project-RecSys/utils.py
@@ -6,6 +6,8 @@ import json
 import pandas as pd
 import os, json, hashlib
 import numpy as np
+import pickle
+
 
 # --- Helper methods for saving and loading hybrid model ---
 def save_hybrid(hybrid, path="artifacts/hybrid_model.joblib"):
@@ -13,7 +15,23 @@ def save_hybrid(hybrid, path="artifacts/hybrid_model.joblib"):
     joblib.dump(hybrid, path)
     print(f"[Hybrid] Saved to {path}")
 
+
 def load_hybrid(path="artifacts/hybrid_model.joblib"):
     hybrid = joblib.load(path)
     print(f"[Hybrid] Loaded from {path}")
     return hybrid
+
+
+def save_embeddings(emb, path):
+    if not path.endswith(".pkl"):
+        path += ".pkl"
+    with open(path, "wb") as f:
+        pickle.dump(emb, f)
+    print(f"Embeddings saved to {path}")
+
+
+def load_embeddings(path):
+    with open(path, "rb") as f:
+        emb = pickle.load(f)
+    print(f"Embeddings loaded from {path}")
+    return emb
